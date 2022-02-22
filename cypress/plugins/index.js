db7f553562122bfe95df73b16785eddc93ef0935
Stephen Cooper <scooperdev@gmail.com>
2022-02-22 16:11:01 +0000

    AG-6378 Read from downloaded file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,82 +28,15 @@ module.exports = (on, config) => {
 
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  config.env.examples = getExamples();
-  config.env.apiPages = getApiDocumentationPages();
+  const configFile = './config/cypress.config.json';
+  if (!fs.existsSync(configFile)) {
+    throw new Error('Missing cypress.config.json file. Please run the `npm run download-cypress-config` task first.')
+  }
+
+  const testConfigFile = fs.readFileSync(configFile);
+  const testConfig = JSON.parse(testConfigFile);
+  config.env.examples = testConfig.examples;
+  config.env.apiPages = testConfig.apiPages;
   return config;
 }
-const basePath = '../ag-grid/grid-packages/ag-grid-docs/documentation/doc-pages/'
-
-function getExamples() {
-  var files = fs.readdirSync(basePath);
-  var pageGroups = [];
-
-  files.forEach(page => {
-    const exampleDir = basePath + page + '/examples/';
-    if (fs.existsSync(exampleDir)) {
-      const examples = fs.readdirSync(exampleDir);
-      let pageExamples = [];
-
-      examples.forEach(example => {
-        const exFolder = exampleDir + '/' + example;
-
-        const generatedFolder = `${exFolder}/_gen`
-        if (fs.existsSync(generatedFolder)) {
-          // Examples follow the _gen generated pattern           
-          ['modules', 'packages'].forEach(importType => {
-            const importTypeFolder = `${exFolder}/_gen/${importType}/`;
-            if (fs.existsSync(importTypeFolder)) {
-              const frameworks = fs.readdirSync(importTypeFolder);
-              frameworks.forEach(framework => {
-                pageExamples.push({ page, example, importType, framework, url: `${page}/${example}/${importType}/${framework}/index.html` });
-              })
-            }
-          })
-        } else {
-
-          // Follows the hand written direct copy examples
-          const customExampleFolder = exampleDir + example;
-
-          const customContents = fs.readdirSync(customExampleFolder);
-          if (customContents.some(f => f === 'app' || !fs.lstatSync(customExampleFolder + '/' + f).isDirectory())) {
-            // if there is a file at this level then assume it is the example code
-            pageExamples.push({ page, example, url: `${page}/${example}/index.html` });
-
-          } else {
-            // assume we have framework folders 
-            const frameworks = fs.readdirSync(customExampleFolder);
-            frameworks.forEach(framework => {
-              pageExamples.push({ page, example, framework, url: `${page}/${example}/${framework}/index.html` });
-            })
-          }
-        }
-      })
-      pageGroups.push({ page: page, examples: pageExamples });
-    }
-  })
-  return pageGroups;
-}
-
-function getApiDocumentationPages() {
-  var files = fs.readdirSync(basePath);
-  var pageGroups = [];
-
-  files.forEach(page => {
-    const indexFilePath = basePath + page + '/index.md';
-    if (fs.existsSync(indexFilePath)) {
-      const indexFile = fs.readFileSync(indexFilePath);
-      if (indexFile.includes('<api-documentation')) {
-        pageGroups.push({ page: page });
-      }
-    }
-  })
-  return pageGroups;
-}
-
-/* 
-function getPages() {
-  const basePath = '../ag-grid/grid-packages/ag-grid-docs/documentation/doc-pages/'
-  var files = fs.readdirSync(basePath);
-  return files;
-} */
 
