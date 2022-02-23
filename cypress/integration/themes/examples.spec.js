471b552d7268fc4e0f45a514cd88266b86628932
Stephen Cooper <scooperdev@gmail.com>
2022-02-23 16:35:30 +0000

    run against all frameworks

--- cypress/integration/themes/examples.spec.js
+++ cypress/integration/themes/examples.spec.js
@@ -9,23 +9,47 @@ describe('AG Grid Examples', {
 }, () => {
 
     const route = 'https://build.ag-grid.com/examples';
+    const frameworks = ['vanilla', 'typescript', 'angular', 'react', 'reactFunctional', 'vue', 'vue3'];
     const examples = [
         'column-pinning/column-pinning/packages/vanilla/index.html',
-        "master-detail-grids/grid-options/modules/vanilla/index.html"
+        "master-detail-grids/grid-options/modules/vanilla/index.html",
+        "sparklines-points-of-interest/sparkline-special-points/modules/vanilla/index.html",
+        "integrated-charts-api-pivot-chart/pivot-chart-api/modules/vanilla/index.html",
+        "filter-multi/floating-filters/packages/vanilla/index.html",
+        "side-bar/fine-tuning/packages/vanilla/index.html",
+        "tool-panel-columns/custom-layout/packages/vanilla/index.html",
+        "tree-data/file-browser/packages/vanilla/index.html",
+        "pivoting/secondary-columns/packages/vanilla/index.html",
+        "grouping-footers/customising-footer-values/packages/vanilla/index.html",
+        "cell-styles/cell-styling/packages/vanilla/index.html",
+        "row-spanning/row-spanning-complex/packages/vanilla/index.html",
+        "component-cell-renderer/cell-renderer/modules/vanilla/index.html",
+        "component-status-bar/custom-component/packages/vanilla/index.html",
+        "component-tool-panel/custom-stats/packages/vanilla/index.html",
+        "aligned-grids/aligned-column-groups/vanilla/index.html"
     ]
 
     Cypress._.forEach(examples, (example) => {
         const testName = example.split('/').splice(0, 2).join('_');
-        it(testName, () => {
-            cy.visit(`${route}/${example}`)
-            cy.wait(5_000)
+        describe(testName, () => {
 
-            // enable viewing test image even if following step fails
-            // This enables us to download the image from TeamCity to then update the snapshot
-            // Local Mac image will be different.
-            const imgName = `${Cypress.platform}/${testName}`;
-            cy.screenshot(`${imgName}_COPY`);
-            cy.matchImageSnapshot(imgName);
+            Cypress._.forEach(frameworks, (framework) => {
+
+                it(framework, () => {
+                    const frameworkExample = example.replace('/vanilla/', `/${framework}/`)
+                    cy.visit(`${route}/${frameworkExample}`)
+                    cy.get('.ag-row', { timeout: 10_000 }).wait(2_000);
+                    //cy.wait(5_000)
+
+                    // enable viewing test image even if following step fails
+                    // This enables us to download the image from TeamCity to then update the snapshot
+                    // Local Mac image will be different.
+                    const imgName = `${Cypress.platform}/${testName}_${framework}`;
+                    cy.screenshot(`${imgName}_COPY`);
+                    cy.matchImageSnapshot(imgName);
+                })
+            });
+
         })
     });
 
