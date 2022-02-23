6328f350061ee7b4d0fe6ad1fc975bf478b41131
Stephen Cooper <scooperdev@gmail.com>
2022-02-23 11:38:00 +0000

    AG-6378 Only picture the grid

--- cypress/integration/themes/demo.spec.js
+++ cypress/integration/themes/demo.spec.js
@@ -7,7 +7,10 @@ describe('AG Grid Demo', () => {
     it('snapshot', () => {
         cy.visit(`https://build.ag-grid.com`)
         cy.wait(5_000)
-        cy.matchImageSnapshot('demo', { capture: 'fullPage' });
+        cy.get('#onetrust-banner-sdk').should('be.visible');
+        cy.get('#onetrust-accept-btn-handler').click();
+        cy.get('#onetrust-banner-sdk').should('not.be.visible');
+        cy.get('#bestHtml5Grid').matchImageSnapshot('demo');
     })
 
 })
