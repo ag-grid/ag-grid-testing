8809b7a24697d6302d3e989d418cc2eb25cf5756
Stephen Cooper <scooperdev@gmail.com>
2023-10-16 17:09:35 +0100

    temp ts only

--- run-config.js
+++ run-config.js
@@ -18,17 +18,16 @@ async function runConfigTests(framework, importType, isCharts, excludeTests) {
 
 ; (async () => {
 
+    await runConfigTests('typescript', 'packages', false, []);
     await runConfigTests('vanilla', 'packages', false, [
         { page: 'component-cell-renderer', example: 'dynamic-components' },
         { page: 'component-filter', example: 'filter-component' },
         { page: 'component-floating-filter', example: 'floating-filter-component' },
         { page: 'rxjs' },
     ]);
-    //await runConfigTests('vanilla', 'packages', true, []);
-    await runConfigTests('typescript', 'packages', false, []);
-    await runConfigTests('angular', 'modules', false, []);
-    await runConfigTests('reactFunctional', 'modules', false, []);
-    await runConfigTests('vue', 'modules', false, []);
-    await runConfigTests('vue3', 'modules', false, []);
+    //await runConfigTests('angular', 'modules', false, []);
+    //await runConfigTests('reactFunctional', 'modules', false, []);
+    //await runConfigTests('vue', 'modules', false, []);
+    //await runConfigTests('vue3', 'modules', false, []);
 
 })()
