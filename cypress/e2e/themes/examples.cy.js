24d8c8f61b5e133be7782d3134c4e3b8908ee5e3
Stephen Cooper <scooperdev@gmail.com>
2023-10-18 13:18:09 +0100

    just work off modules

--- cypress/e2e/themes/examples.cy.js
+++ cypress/e2e/themes/examples.cy.js
@@ -11,25 +11,25 @@ describe('AG Grid Examples',
         const envFramework = Cypress.env('framework');
 
         const route = 'https://build.ag-grid.com/examples';
-        const frameworks = envFramework ? [envFramework] : ['vanilla', 'typescript', 'angular', 'react', 'reactFunctional', 'vue', 'vue3'];
+        const frameworks = envFramework ? [envFramework] : ['vanilla', 'typescript', 'angular', 'reactFunctional', 'reactFunctionalTs', 'vue', 'vue3'];
         const examples = [
-            "column-pinning/column-pinning/packages/vanilla/index.html",
+            "column-pinning/column-pinning/modules/vanilla/index.html",
             "master-detail-grids/grid-options/modules/vanilla/index.html",
             "sparklines-points-of-interest/sparkline-special-points/modules/vanilla/index.html",
             // TODO: AG-6790 - re-enable after Integrated Charts -> Charts integration rework.
             // "integrated-charts-api-pivot-chart/pivot-chart-api/modules/vanilla/index.html",
-            "filter-multi/floating-filters/packages/vanilla/index.html",
-            "side-bar/fine-tuning/packages/vanilla/index.html",
-            "tool-panel-columns/custom-layout/packages/vanilla/index.html",
-            "tree-data/file-browser/packages/vanilla/index.html",
-            "pivoting/secondary-columns/packages/vanilla/index.html",
-            "grouping-footers/customising-footer-values/packages/vanilla/index.html",
-            "cell-styles/cell-styling/packages/vanilla/index.html",
-            "row-spanning/row-spanning-complex/packages/vanilla/index.html",
+            "filter-multi/floating-filters/modules/vanilla/index.html",
+            "side-bar/fine-tuning/modules/vanilla/index.html",
+            "tool-panel-columns/custom-layout/modules/vanilla/index.html",
+            "tree-data/file-browser/modules/vanilla/index.html",
+            "pivoting/secondary-columns/modules/vanilla/index.html",
+            "grouping-footers/customising-footer-values/modules/vanilla/index.html",
+            "cell-styles/cell-styling/modules/vanilla/index.html",
+            "row-spanning/row-spanning-complex/modules/vanilla/index.html",
             "component-cell-renderer/cell-renderer/modules/vanilla/index.html",
-            "component-status-bar/custom-component/packages/vanilla/index.html",
-            "component-tool-panel/custom-stats/packages/vanilla/index.html",
-            "aligned-grids/aligned-column-groups/packages/vanilla/index.html"
+            "component-status-bar/custom-component/modules/vanilla/index.html",
+            "component-tool-panel/custom-stats/modules/vanilla/index.html",
+            "aligned-grids/aligned-column-groups/modules/vanilla/index.html"
         ]
 
         Cypress._.forEach(examples, (example) => {
@@ -41,7 +41,7 @@ describe('AG Grid Examples',
                     it(framework, () => {
                         const frameworkExample = example.replace('/vanilla/', `/${framework}/`)
                         cy.visit(`${route}/${frameworkExample}`)
-                        cy.get('.ag-row', { timeout: 10_000 }).wait(3_000);
+                        cy.get('.ag-root-wrapper', { timeout: 10_000 }).get('.ag-body-viewport', { timeout: 1_000 }).wait(3_000);
 
                         // enable viewing test image even if following step fails
                         // This enables us to download the image from TeamCity to then update the snapshot
