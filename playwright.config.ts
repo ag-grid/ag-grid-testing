7583264301dba7eb23f44c0b5e141fae8e22a989
Stephen Cooper <scooperdev@gmail.com>
2024-03-05 20:51:11 +0000

    update CI config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,14 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  timeout: 5_000,
+  timeout: 10_000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
+  workers: undefined, // try parallel tests on CI
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,7 +30,7 @@ export default defineConfig({
     baseURL: process.env.CI ? 'https://grid-staging.ag-grid.com' : 'https://localhost:4610',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'off' : 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
