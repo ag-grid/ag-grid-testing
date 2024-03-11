b92641dfacb6c05eb98a22287794640e141699c6
Stephen Cooper <scooperdev@gmail.com>
2024-03-11 22:34:20 +0000

    let CI decide no. workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
+  workers: undefined,
   /* Limit parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  //workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
