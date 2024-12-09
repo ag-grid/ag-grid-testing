f3814ea22e4d29f7844ba80de1b967d9c573a8e2
Stephen Cooper <scooperdev@gmail.com>
2024-12-09 16:49:37 +0000

    RC v33 changes

--- tests/exampleTestRunner.ts
+++ tests/exampleTestRunner.ts
@@ -53,7 +53,7 @@ export function getSelectionOfFrameworkExamples(
 
 export function getExampleConfig(e, importType: undefined | 'packages' | 'modules' = undefined) {
   const examplePath = `${e.pageName}/${e.exampleName}/${importType ? importType + '/': ''}${e.internalFramework}`;
-  const url = `/examples/${examplePath}/`;
+  const url = `/archive/33.0.0/examples/${examplePath}/`;
   return { examplePath, url };
 }
 
